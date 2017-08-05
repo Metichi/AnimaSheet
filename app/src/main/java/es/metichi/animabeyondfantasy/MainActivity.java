34fc47e14506b8e021ecbceb6c907ec537303667
metichi <metichi@gmail.com>
2017-08-05 18:29:34 +0200

    Creado menu con header

--- app/src/main/java/es/metichi/animabeyondfantasy/MainActivity.java
+++ app/src/main/java/es/metichi/animabeyondfantasy/MainActivity.java
@@ -1,13 +1,39 @@
 package es.metichi.animabeyondfantasy;
 
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+    private String[] mSheetTabs;
+    private DrawerLayout mDrawerLayout;
+    private ListView mDrawerList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mSheetTabs = getResources().getStringArray(R.array.sheetTabs);
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        mDrawerList = (ListView) findViewById(R.id.left_drawer);
+
+        mDrawerList.setAdapter(new ArrayAdapter<String>(this,R.layout.menu_items,mSheetTabs));
+        mDrawerList.setOnItemClickListener(new DrawerItemClickListener());
+        mDrawerList.addHeaderView(getLayoutInflater().inflate(R.layout.menu_header,null));
     }
+
+    private class DrawerItemClickListener implements ListView.OnItemClickListener{
+        @Override
+        public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+            selectItem(i);
+        }
+    }
+    private void selectItem(int position){
+
+    }
+
 }
