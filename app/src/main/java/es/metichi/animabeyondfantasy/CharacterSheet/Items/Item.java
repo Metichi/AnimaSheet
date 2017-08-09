9ca7899e1100674c5168a6bcffb7fbbd81d547df
Metichi <metichi@gmail.com>
2017-08-09 13:47:12 +0200

    Implemented Currency

--- app/src/main/java/es/metichi/animabeyondfantasy/CharacterSheet/Items/Item.java
+++ app/src/main/java/es/metichi/animabeyondfantasy/CharacterSheet/Items/Item.java
@@ -8,7 +8,7 @@ public class Item {
     protected String name;
     protected String description;
     protected float weight;
-    protected float value;
+    protected Currency value;
     protected int quality;
     protected int presence;
     public enum Aviability{
@@ -18,7 +18,7 @@ public class Item {
     }
     protected Aviability aviability;
 
-    public Item(String name, String description, float weight, float value, int quality){
+    public Item(String name, String description, float weight, Currency value, int quality){
         this.name = name;
         this.description = description;
         this.weight = weight;
@@ -26,7 +26,7 @@ public class Item {
         this.quality = quality;
         this.aviability = Aviability.COMMON;
     }
-    public Item(String name, String description, float weight, float value, int quality, Aviability aviability){
+    public Item(String name, String description, float weight, Currency value, int quality, Aviability aviability){
         this(name,description,weight,value,quality);
         this.aviability = aviability;
     }
@@ -35,7 +35,7 @@ public class Item {
         this.name = name;
     }
 
-    public void setValue(float value) {
+    public void setValue(Currency value) {
         this.value = value;
     }
 
@@ -51,7 +51,7 @@ public class Item {
         return name;
     }
 
-    public float getValue() {
+    public Currency getValue() {
         return value;
     }
 
@@ -68,9 +68,114 @@ public class Item {
     }
 
     public static class Weapon extends Item{
-        public Weapon(String name, String description, float weight, float value, int quality){
+        public Weapon(String name, String description, float weight, Currency value, int quality){
             super(name,description, weight, value, quality);
         }
         //TODO: Complete weapon class.
+    }
+
+    /**
+     * Class to represent an ammount of money
+     *
+     * This class holds an ammount of money and contains methods to get the stored ammount, compare
+     * it to another and change the coins into bigger coins.
+      */
+    public static class Currency{
+        private int copperPieces;
+        private int silverPieces;
+        private int goldPieces;
+
+        public Currency(int gold,int silver, int copper){
+            this.goldPieces = gold;
+            this.silverPieces = silver;
+            this.copperPieces = copper;
+        }
+        public Currency(int totalCopper){
+            int copper = totalCopper%10;
+            int remaining = totalCopper/10;
+            int silver = remaining%100;
+            int gold = remaining/100;
+
+            this.copperPieces=copper;
+            this.silverPieces=silver;
+            this.goldPieces=gold;
+        }
+
+        public void addCopperPieces(int c){
+            this.copperPieces += c;
+        }
+        public void addSilverPieces(int c){
+            this.silverPieces += c;
+        }
+        public void addGoldPieces(int c){
+            this.goldPieces += c;
+        }
+
+        public int getCopperPieces() {
+            return copperPieces;
+        }
+
+        public int getGoldPieces() {
+            return goldPieces;
+        }
+
+        public int getSilverPieces() {
+            return silverPieces;
+        }
+
+        public void setCopperPieces(int copperPieces) {
+            this.copperPieces = copperPieces;
+        }
+
+        public void setGoldPieces(int goldPieces) {
+            this.goldPieces = goldPieces;
+        }
+
+        public void setSilverPieces(int silverPieces) {
+            this.silverPieces = silverPieces;
+        }
+
+        public int getTotatlCopperPieces(){
+            int totalCopperPieces = getCopperPieces();
+            totalCopperPieces += getSilverPieces()*10;
+            totalCopperPieces += getGoldPieces()*1000;
+            return totalCopperPieces;
+        }
+        public void changeCurrency(){
+            int remainder = getCopperPieces()%10;
+            addSilverPieces(getCopperPieces()/10);
+            setCopperPieces(remainder);
+
+            remainder = getSilverPieces()%100;
+            addGoldPieces(getSilverPieces()/100);
+            setSilverPieces(remainder);
+        }
+
+        public void add(Currency c){
+            addCopperPieces(c.getCopperPieces());
+            addSilverPieces(c.getSilverPieces());
+            addGoldPieces(c.getGoldPieces());
+        }
+        public void remove(Currency c){
+            addCopperPieces(-c.getCopperPieces());
+            addSilverPieces(-c.getSilverPieces());
+            addGoldPieces(-c.getGoldPieces());
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            if (obj instanceof Currency){
+                return getTotatlCopperPieces() == ((Currency)obj).getTotatlCopperPieces();
+            }else{
+                return false;
+            }
+        }
+
+        public boolean greaterThan(Currency c){
+            return getTotatlCopperPieces() > c.getTotatlCopperPieces();
+        }
+        public boolean lesserThan(Currency c){
+            return getTotatlCopperPieces() < c.getTotatlCopperPieces();
+        }
     }
 }
